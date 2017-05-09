b0595cf8813b076e0e1c08e852ecf55b743d012b
silvana <sr677@njit.edu>
2017-05-09 19:52:01 -0400

    formating fix

--- useddaysmongo.php
+++ useddaysmongo.php
@@ -5,20 +5,27 @@
 
 	$command = new MongoDB\Driver\Command(['ping' => 1]);
 	$mdb->executeCommand ('db', $command);
-	$servers = $mdb->getServers();
-	print_r($servers);   
+	$servers = $mdb->getServers();   
 	$filter = array();
-	$options = ['sort' => ['total' => 1]];
-	$query = new MongoDB\Driver\Query($filter);
+	$options = ['sort' => ['total' => -1]];
+	$query = new MongoDB\Driver\Query($filter, $options);
 	$results = $mdb->executeQuery("payroll.vacation", $query);
-	print_r($results->toArray());
+
+foreach ($results as $doc) {
+	$employeeid = $doc->employeeid;
+	$vacation_used = $doc->vacation_used;
+	$sick_used = $doc->sick_used;
+	$excused_used = $doc->excused_used;
+	$total = $doc->total;	
 	
+echo "EmployeeID:".$employeeid.","."Vacation days used:".$vacation_used.","."Sick days used".$sick_used.","."Excused days used".$excused_used.","."Total days used:".$total;
+
+echo"\r\n";
+echo "\r\n";
+	}
 }
 catch(exception $e)
 {
-	print_r($e);
+print_r($e);
 }
 ?>
-
-
-
